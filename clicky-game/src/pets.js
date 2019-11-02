2a261429f836871b503305df89057fbe2692ce64
Melinda Santana <melinda.santana@gmail.com>
2019-11-01 17:28:23 -0700

    updating pet urls with multiple images
    updating pet urls with multiple images

--- clicky-game/src/pets.js
+++ clicky-game/src/pets.js
@@ -17,35 +17,35 @@ const pets=[
     },
     {
         id: 5,
-        img: "https://www.petmd.com/sites/default/files/introduce-dog-to-cat.jpg"
+        img: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12193133/German-Shepherd-Puppy-Fetch.jpg"
     },
     {
         id: 6,
-        img: "https://www.petmd.com/sites/default/files/introduce-dog-to-cat.jpg"
+        img: "https://www.thesprucepets.com/thmb/tDAQylKuZ2PeKgRaVe3O06jjMxc=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/frenchie-grass-FrankGaglione-Getty-56af40303df78cf772c21867.jpg"
     },
     {
         id: 7,
-        img: "https://www.petmd.com/sites/default/files/introduce-dog-to-cat.jpg"
+        img: "https://www.thesprucepets.com/thmb/hy0vWBBxqKUa_1SyVaXTRGQSNIc=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-686801347-599df88322fa3a0011fd61e1.jpg"
     },
     {
         id: 8,
-        img: "https://www.petmd.com/sites/default/files/introduce-dog-to-cat.jpg"
+        img: "https://www.thesprucepets.com/thmb/J8q2WuIr1wSiJOwEKMk_klGgrAE=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-98843439-a4dddb22285a4150a858f829b52f8f31.jpg"
     },
     {
         id: 9,
-        img: "https://www.petmd.com/sites/default/files/introduce-dog-to-cat.jpg"
+        img: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2018/05/02112816/puppy-with-cat-header.jpg"
     },
     {
         id: 10,
-        img: "https://www.petmd.com/sites/default/files/introduce-dog-to-cat.jpg"
+        img: "https://s.abcnews.com/images/Lifestyle/cats-dogs2-gty-mem-171130_4x3_992.jpg"
     },
     {
         id: 11,
-        img: "https://www.petmd.com/sites/default/files/introduce-dog-to-cat.jpg"
+        img: "https://s.abcnews.com/images/Lifestyle/cats-dogs1-gty-mem-171130_4x3_992.jpg"
     },
     {
         id: 12,
-        img: "https://www.petmd.com/sites/default/files/introduce-dog-to-cat.jpg"
+        img: "https://s.abcnews.com/images/Lifestyle/cats-dogs3-gty-mem-171130_4x3_992.jpg"
     },
 ]
 module.exports=pets
